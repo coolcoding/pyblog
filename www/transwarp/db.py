1ceccea59ebecd0460eaf499209abc53346ae415
coolcoding <1029884576@163.com>
2014-11-11 08:05:05 +0800

    update orm

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -104,10 +104,100 @@ class _DbCtx(threading.local):
 
     def cursor(self):
         return self.connection.cursor()
+_db_ctx = _DbCtx()
+
+class _ConnectionCtx(object):
+
+    def __enter__(self):
+        global _db_ctx
+        self.should_cleanup = False
+        if not _db_ctx.isInit():
+            _db_ctx.init()
+            self.should_cleanup = True
+        return self
+
+    def __exit__(self,exctype,excvalue,traceback):
+        global _db_ctx
+        if self.should_cleanup:
+            _db_ctx.cleanup()
+
+def connection():
+    return _ConnectionCtx()
+
+
+
+def with_connection(func):
+
+    @functools.wraps(func)
+    def _wrapper(*args, **kw):
+        with _ConnectionCtx():
+            return func(*args, **kw)
+    return _wrapper
+
+
+def _select(sql,first,*args):
+
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?','%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql,args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql,args)
+        if cursor.description:
+            names = [x[0] for x in cursor.description]
+        if first:
+            values = cursor.fetchone()
+            if not values:
+                return None
+            return Dict(names,values)
+        return [Dict(names,x) for x in cursor.fetchall()]
+    finally:
+        if cursor:
+            cursor.close()
+
+@with_connection
+def select_one(sql, *args):
+
+    return _select(sql, True, *args)
+
+
+@with_connection
+def select(sql, *args):
+
+    return _select(sql,False,*args)
+
+@with_connection
+def _update(sql,*args):
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?','%s')
+    logging.info('SQL: %s ARGS: %s' % (sql,args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql,args)
+        r = cursor.rowcount
+        return r
+    finally:
+        if cursor:
+            cursor.close()
+
+
+def update(sql, *args):
+
+    return _update(sql, *args)
+
+
+def insert(table, **kw):
+
+    cols, args = zip(*kw.iteritems())
+    sql = 'insert into `%s` (%s) values(%s)' % (table,','.join(['`%s`' % col for col in cols]),','.join(['?' for i in range(len(cols))]))
+    return _update(sql, *args)
 
 
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    create_engine('root','root','')
+    create_engine('root','root','falcon')
+    print select('select * from stat limit 3')
